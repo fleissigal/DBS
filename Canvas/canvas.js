18dd5688a03831866b451c1cbbe4da4e8cdce89b
galfleissig <fleissigal@gmail.com>
2017-12-30 23:36:34 -0500

    Added the use of JQuery and completed all the helper functions to draw the shapes

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -1,44 +1,86 @@
-function draw() {
+// function draw() {
+// 	document.write(5);
+// }
+$(document).ready(function(){
 
-	var canvas = document.getElementById('myCanvas');
+	$("#drawButton").click(function() {
 
-	if (canvas.getContext) {
-		var ctx = canvas.getContext('2d');
+		var canvas = $("#myCanvas")[0];
 
-		// Squares
-		ctx.fillStyle = 'rgb(0, 0, 255)'; // Blue color
-		ctx.fillRect(10, 10, 10, 10);
+		if (canvas.getContext) {
+			var ctx = canvas.getContext('2d');
 
-		ctx.fillStyle = 'rgb(255, 0, 0)'; // Red color
-		ctx.fillRect(20, 20, 20, 20);
+			
 
-		ctx.fillStyle = 'rgb(255, 255, 0)'; // Yellow color
-		ctx.fillRect(30, 30, 30, 30);
+			draw("square", 2, "red");
+			draw("triangle", 3, "yellow");
+			draw("circle", 2, "blue");
+		}
+	});
+});
 
-		// Triangle
-		ctx.beginPath();
-	    ctx.moveTo(75, 50);
-	    ctx.lineTo(100, 75);
-	    ctx.lineTo(100, 25);
-	    ctx.fill();
+function draw(shape, number, color) {
+	var canvas = $("#myCanvas")[0];
+	var ctx = canvas.getContext('2d');
 
-	    // Circle
-		var circle = new Path2D();
-		circle.moveTo(100, 100);
-		circle.arc(100, 35, 25, 0, 2*Math.PI);
-		ctx.fill(circle);
+	switch(color) {
+		case "blue":
+			ctx.fillStyle = 'rgb(0, 0, 255)'; // Blue color
+			break;
+		case "red":
+			ctx.fillStyle = 'rgb(255, 0, 0)'; // Red color
+			break;
+		case "yellow":
+			ctx.fillStyle = 'rgb(255, 255, 0)'; // Yellow color
+			break;
+		default:
+			break;
+	}
 
+	switch(shape) {
+		case "circle":
+			drawCircle(number);
+			break;
+		case "square":
+			drawSquare(number);
+			break;
+		case "triangle":
+			drawTriangle(number);
+			break;
+		default:
+			break;
 	}
 }
 
-function drawCircle(ctx, color, number) {
+function drawCircle(number) {
+	var canvas = $("#myCanvas")[0];
+	var ctx = canvas.getContext('2d');
 
+	for (i = 1; i < number + 1; i++) {
+		ctx.beginPath();
+		ctx.arc(i * 100, 100, 30, 0, 2*Math.PI);
+		ctx.fill();
+	}
 }
 
-function drawSquare(ctx, color, number) {
+function drawSquare(number) {
+	var canvas = $("#myCanvas")[0];
+	var ctx = canvas.getContext('2d');
 
+	for (i = 1; i < number + 1; i++) {
+		ctx.fillRect(i * 10, i * 10, i * 10, i * 10);
+	}
 }
 
-function drawTriangle(ctx, color, number) {
+function drawTriangle(number) {
+	var canvas = $("#myCanvas")[0];
+	var ctx = canvas.getContext('2d');
 
+	for (i = 1; i < number + 1; i++) {
+		ctx.beginPath();
+	    ctx.moveTo(i * 80, i * 80);
+	    ctx.lineTo((i * 80) + 30 , i * 80);
+	    ctx.lineTo((i * 80) + 15, (i * 80) - 30);
+	    ctx.fill();
+	}
 }
