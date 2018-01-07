4c02e1c0a9405313b051e21d304b17b5addc3760
galfleissig <fleissigal@gmail.com>
2018-01-07 18:21:22 -0500

    Improved the app to be able to select 3 different shapes and 8 different colors and they change immediately

--- sample_apps/ThreeJS_app/myApp.js
+++ sample_apps/ThreeJS_app/myApp.js
@@ -1,4 +1,4 @@
-//1
+//1 - setup of the environment
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -7,26 +7,40 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//2
+var color = "red";
+var shapeName = "cube";
+//2 - default shape
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: "red" } );
-var cube = new THREE.Mesh( geometry, material );
-cube.name = "myCube";
-
-scene.add( cube );
-
-camera.position.z = 3;
+var material = new THREE.MeshBasicMaterial( {color: "red"} );
+var shape = new THREE.Mesh( geometry, material );
+shape.name = "myShape";
+shape.position.set(0, 0, 0);
+scene.add( shape );
+camera.position.z = 5;
 camera.position.y = 1;
+
+// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+// var material = new THREE.MeshBasicMaterial( {color: "blue"} );
+// var shape2 = new THREE.Mesh( geometry, material );
+// shape2.name = "myShape2";
+// shape2.position.set(3, 0, 0);
+// scene.add( shape2 );
+// camera.position.z = 5;
+// camera.position.y = 1;
 
 //3
 
-function animate(object) {
+function animate() {
 	requestAnimationFrame( animate );
 	//4
-	cube.rotation.x += 0.00;
-	cube.rotation.y += 0.01;
 
+	shape.rotation.x += 0.0;
+	shape.rotation.y += 0.005;
+
+	// shape2.rotation.x += 0.0;
+	// shape2.rotation.y += 0.005;
+	
 	renderer.render( scene, camera );
 }
 
@@ -38,40 +52,63 @@ function removeObject(objectName) {
 	scene.remove(objectToDelete);
 }
 
+function changeColor() {
+	removeObject("myShape");
+	if (shapeName == "sphere") {
+		material = new THREE.MeshBasicMaterial( {color: color, wireframe:true} );
+	} else {
+		material = new THREE.MeshBasicMaterial( {color: color} );
+	}
+	shape = new THREE.Mesh( geometry, material );
+	shape.position.set(0, 0, 0);
+	shape.name = "myShape";
+	scene.add( shape );
+}
 
-function changeColor(chosenColor) {
+function changeObject() {
 
-	removeObject("myCube");
+	removeObject("myShape");
 
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-	var material = new THREE.MeshBasicMaterial( { color: chosenColor } );
-	cube = new THREE.Mesh( geometry, material );
-	cube.name = "myCube";
+	if (shapeName == "cube") {
+		geometry = new THREE.BoxGeometry( 1, 1, 1 );
+		material = new THREE.MeshBasicMaterial( {color: color} );
+	}
 
-	scene.add( cube );
+	if (shapeName == "sphere") {
+		geometry = new THREE.SphereGeometry(0.7, 50, 50, 0, Math.PI * 2, 0, Math.PI * 2);
+		material = new THREE.MeshBasicMaterial( {color: color, wireframe:true} );
+	}
 
-	camera.position.z = 3;
+	if (shapeName == "pyramid") {
+		geometry = new THREE.CylinderGeometry( 0, 1, 1, 4 );
+		material = new THREE.MeshBasicMaterial( {color: color} );
+
+	}
+
+	shape = new THREE.Mesh( geometry, material );
+	shape.position.set(0, 0, 0);
+	shape.name = "myShape";
+	scene.add( shape );
+	camera.position.z = 5;
 	camera.position.y = 1;
-
 }
 
 
 $(document).ready(function(){
 
-	// This function calls a helper function to draw shapes on
-	// the canvas when the "drawButton" button is clicked
-	$("#red").click(function() {
-		changeColor("red");
+	$('.shapeButton').click(function() {
+		shapeName = $(this).val();
+		changeObject();
 		animate();
 	});
-	$("#blue").click(function() {
-		changeColor("blue");
+
+
+	$('.colorButton').click(function() {
+		color = $(this).val();
+		changeColor();
 		animate();
 	});
-	$("#yellow").click(function() {
-		changeColor("yellow");
-		animate();
-	});
+
 });
 
 
