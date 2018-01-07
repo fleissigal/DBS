380ce1712899fe4903ae6fb49480a070c0824275
galfleissig <fleissigal@gmail.com>
2018-01-07 13:07:52 -0500

    Edited the sample app so that the color of the cube can be changed

--- sample_apps/ThreeJS_app/myApp.js
+++ sample_apps/ThreeJS_app/myApp.js
@@ -10,8 +10,10 @@ document.body.appendChild( renderer.domElement );
 //2
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0xc70039 } );
+var material = new THREE.MeshBasicMaterial( { color: "red" } );
 var cube = new THREE.Mesh( geometry, material );
+cube.name = "myCube";
+
 scene.add( cube );
 
 camera.position.z = 3;
@@ -19,7 +21,7 @@ camera.position.y = 1;
 
 //3
 
-function animate() {
+function animate(object) {
 	requestAnimationFrame( animate );
 	//4
 	cube.rotation.x += 0.00;
@@ -27,4 +29,51 @@ function animate() {
 
 	renderer.render( scene, camera );
 }
+
 animate();
+
+
+function removeObject(objectName) {
+	var objectToDelete = scene.getObjectByName(objectName);
+	scene.remove(objectToDelete);
+}
+
+
+function changeColor(chosenColor) {
+
+	removeObject("myCube");
+
+	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+	var material = new THREE.MeshBasicMaterial( { color: chosenColor } );
+	cube = new THREE.Mesh( geometry, material );
+	cube.name = "myCube";
+
+	scene.add( cube );
+
+	camera.position.z = 3;
+	camera.position.y = 1;
+
+}
+
+
+$(document).ready(function(){
+
+	// This function calls a helper function to draw shapes on
+	// the canvas when the "drawButton" button is clicked
+	$("#red").click(function() {
+		changeColor("red");
+		animate();
+	});
+	$("#blue").click(function() {
+		changeColor("blue");
+		animate();
+	});
+	$("#yellow").click(function() {
+		changeColor("yellow");
+		animate();
+	});
+});
+
+
+
+
