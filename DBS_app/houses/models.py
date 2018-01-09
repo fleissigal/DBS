8a905197f88ce10bff9d86071d957aec3318eab5
galfleissig <fleissigal@gmail.com>
2018-01-09 18:56:45 -0500

    Fixed the models definitions

--- DBS_app/houses/models.py
+++ DBS_app/houses/models.py
@@ -5,36 +5,9 @@ from django.db import models
 
 # The models of the application
 
-class HousePlan(models.Model):
-	description = models.CharField(max_length = 1000)
-	floorPlan = models.ForeignKey(FloorPlan)
-	houseConfiguration = models.ForeignKey(HouseConfiguration)
-
-class FloorPlan(models.Model):
-	description = models.CharField(max_length = 1000)
-	roomPlan = models.ForeignKey(RoomPlan)
-	floorConfiguration = models.ForeignKey(FloorConfiguration)
-
-class RoomPlan(models.Model):
-	description = models.CharField(max_length = 1000)
-	roomConfiguration = models.ForeignKey(RoomConfiguration)
-
-
-
-class HouseConfiguration(models.Model):
-	optionChoice = models.ManyToManyField(Option)
-
-class FloorConfiguration(models.Model):
-	optionChoice = models.ManyToManyField(Option)
-
-class RoomConfiguration(models.Model):
-	optionChoice = models.ManyToManyField(Option)
-
-
-
 
 class OptionType(models.Model):
-
+	cabinets = models.CharField(max_length = 1000)
 
 class Option(models.Model):
 	optionTypeID = models.ForeignKey(OptionType)
@@ -42,8 +15,36 @@ class Option(models.Model):
 # Need to add a class for OptionChoice ?
 
 
+### Configurations
+
+class HouseConfiguration(models.Model):
+	optionChoice = models.ManyToManyField(Option)
+	housePlan = models.ForeignKey(HousePlan)
+
+class FloorConfiguration(models.Model):
+	optionChoice = models.ManyToManyField(Option)
+	floorPlan = models.ForeignKey(FloorPlan)
+	houseConfiguration = models.ForeignKey(HouseConfiguration)
+
+class RoomConfiguration(models.Model):
+	optionChoice = models.ManyToManyField(Option)
+	roomPlan = models.ForeignKey(RoomPlan)
+	floorConfiguration = models.ForeignKey(FloorConfiguration)
+
+
+### Plans
+
+class HousePlan(models.Model):
+	description = models.CharField(max_length = 1000)
+
+class FloorPlan(models.Model):
+	description = models.CharField(max_length = 1000)
+	housePlan = models.ForeignKey(HousePlan)
+
+class RoomPlan(models.Model):
+	description = models.CharField(max_length = 1000)
+	floorPlan = models.ForeignKey(FloorPlan)
 
 
 
 
-	aaa = models.ManyToManyField(RoomPlan)
