f09a0c54441d7f5696fc4f8b3bd18a0218f37867
galfleissig <fleissigal@gmail.com>
2018-02-09 12:14:48 -0500

    Added a standalone build of the game for Mac)

--- Unity/Roll a ball/Assets/Scripts/PlayerController.cs
+++ Unity/Roll a ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
 	private int count;
 
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText(); // count must hold some value for the ToString function to work
+		winText.text = "";
 	}
 
 	void FixedUpdate() {
@@ -25,6 +31,16 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Pick Up")) {
 			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	// Both count and countText are global variables for the whole script, so we don't need to pass them as arguments
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString();
+		if (count >= 12) {
+			winText.text = "You Win!";
 		}
 	}
 
