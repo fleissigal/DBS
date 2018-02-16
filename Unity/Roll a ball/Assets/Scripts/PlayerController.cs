c4e40ea7e3af9a8bd1daa25e23dd42025637c5ad
galfleissig <fleissigal@gmail.com>
2018-02-16 12:24:41 -0500

    Added 3 ways in which the 'player' color is changed

--- Unity/Roll a ball/Assets/Scripts/PlayerController.cs
+++ Unity/Roll a ball/Assets/Scripts/PlayerController.cs
@@ -8,17 +8,26 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public Text countText;
 	public Text winText;
+	// public Dropdown dropdown;
 
 	private Rigidbody rb;
 	private int count;
 	private Renderer rn;
 	private float elapsedTime;
 	
+	List<string> colors = new List<string>() { "Red", "Green"};
+
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
 		count = 0;
 		SetCountText(); // count must hold some value for the ToString function to work
 		winText.text = "";
+		rn = GetComponent<Renderer> ();
+	}
+
+	// Changes the color of the 'player' from green to red and back according to the dropdown menu
+	public void Dropdown_IndexChanged(int index) {
+		rn.material.color = (index == 0) ? Color.red : Color.green;
 	}
 
 	void FixedUpdate() {
@@ -46,11 +55,12 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
+	// Changes the color of the 'player' from green to red and back every X seconds
 	void Update() {
 	    elapsedTime += Time.deltaTime;
 
-	    if (elapsedTime >= 5) {
-	        elapsedTime -= 5;
+	    if (elapsedTime >= 3) {
+	        elapsedTime -= 3;
 			rn = GetComponent<Renderer> ();
 			if (rn.material.color == Color.red) {
 				rn.material.color = Color.green;
@@ -58,6 +68,11 @@ public class PlayerController : MonoBehaviour {
 				rn.material.color = Color.red;
 			}
 		}
+	}
+
+	// Changes the color of the 'player' from green to red and back according to the toggle
+	public void toggleColor(bool val) {
+		rn.material.color = (val == true) ? Color.red : Color.green;
 	}
 
 }
