d8b91d109a1e51dff504b884b8a85dc43b8fc5b9
galfleissig <fleissigal@gmail.com>
2018-02-20 10:01:28 -0500

    Added a script for the west wall

---
+++ Unity/Roll a ball/Assets/Scripts/WestWallScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WestWallScript : MonoBehaviour {
+
+	private Renderer rn;
+	private float elapsedTime;
+
+
+	// Use this for initialization
+	void Start () {
+		rn = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	// Changes the color of the 'player' from green to red and back every X seconds
+	void Update() {
+	    elapsedTime += Time.deltaTime;
+
+	    if (elapsedTime >= 3) {
+	        elapsedTime -= 3;
+			rn = GetComponent<Renderer> ();
+			if (rn.material.color == Color.red) {
+				rn.material.color = Color.green;
+			} else {
+				rn.material.color = Color.red;
+			}
+		}
+	}
+}
