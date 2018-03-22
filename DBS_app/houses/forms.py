fe6b1258dfef7137ce681f3e45da047cb530e1e8
galfleissig <fleissigal@gmail.com>
2018-03-22 13:07:21 -0400

    Added login and registration functionality

--- DBS_app/houses/forms.py
+++ DBS_app/houses/forms.py
@@ -1,5 +1,67 @@
 from django import forms
 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate as auth_authenticate
+
+
 class UploadFileForm(forms.Form):
-    title = forms.CharField(max_length=50)
+	title = forms.CharField(max_length=50)
 	picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'form-control', 'placeholder':'Upload Picture', 'style':'text-align: center;'}))
+
+class Registration(forms.Form):
+
+	username = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username', 'style':'text-align: center;'}))
+	password = forms.CharField(max_length = 100, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password', 'style':'text-align: center;'}))
+	reEnterPassword = forms.CharField(max_length = 100, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Re-enter Password', 'style':'text-align: center;'}))
+
+	def clean(self):
+		cleaned_data = super(Registration, self).clean()
+
+		password = cleaned_data.get('password')
+		reEnterPassword = cleaned_data.get('reEnterPassword')
+
+		if password and reEnterPassword and password != reEnterPassword:
+			raise forms.ValidationError("Passwords don't match")
+
+		return cleaned_data
+
+
+	def clean_username(self):
+		username = self.cleaned_data.get('username')
+
+		if User.objects.filter(username__exact=username):
+			raise forms.ValidationError("Username is already taken")
+		return username
+
+
+class Login(forms.Form):
+	username = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username', 'style':'text-align: center;'}))
+	password = forms.CharField(max_length = 100, widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password', 'style':'text-align: center;'}))
+
+	def clean(self):
+		cleaned_data = super(Login, self).clean()
+
+		username = cleaned_data.get('username')
+		password = cleaned_data.get('password')
+
+		if User.objects.filter(username__exact=username):
+
+			user = User.objects.get(username=username)
+			if not user.is_active:
+				raise forms.ValidationError("The account has not been activated. Please check email")
+
+			logged_in_user = auth_authenticate(username=username, password=password)
+			if logged_in_user == None:
+				raise forms.ValidationError("Incorrect password")
+
+		return cleaned_data
+
+
+	def clean_username(self):
+		username = self.cleaned_data.get('username')
+		
+		if not User.objects.filter(username__exact=username):
+			raise forms.ValidationError("No such username")
+
+		return username
+
--- DBS_app/houses/views.py
+++ DBS_app/houses/views.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
+from django.core.exceptions import ObjectDoesNotExist
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import authenticate as auth_authenticate
+from django.contrib.auth import logout as auth_logout
+
 from houses.models import *
-# from houses.forms import *
+from houses.forms import *
 
 # Create your views here.
 
@@ -27,6 +36,7 @@ def uploadFile(request):
 
 # This function is responsible for rendering the page with the correct
 # data from the request (the data is being pulled form the DB)
+@login_required()
 def configurator(request, houseID, floorID, roomID):
 
 	context = {}
@@ -40,7 +50,6 @@ def configurator(request, houseID, floorID, roomID):
 				optionsToLoad = optionsToLoad + "-"
 			optionsToLoad = optionsToLoad + option
 
-
 	# Getting the information from the DB
 	model = get_object_or_404(HousePlan, id=houseID)
 	floor = get_object_or_404(FloorPlan, id=floorID)
@@ -51,5 +60,64 @@ def configurator(request, houseID, floorID, roomID):
 
 	context = {'model':model, 'floor':floor, 'room':room, 'rooms':rooms, 'optionTypes':optionTypes , "optionsToLoad":optionsToLoad }
 	return render(request, 'index.html', context)
+
+
+def login(request):
+
+	context = {}
+
+	# In case the user reloaded the page
+	if request.method == 'GET':
+		context['form'] = Login()
+		return render(request, 'login.html', context)
+
+	form = Login(request.POST)
+	context['form'] = form
+
+	if not form.is_valid():
+		return render(request, 'login.html', context)
+
+	logged_in_user = auth_authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+	auth_login(request, logged_in_user)
+
+	model = get_object_or_404(HousePlan, id=1)
+	floor = get_object_or_404(FloorPlan, id=1)
+	room = get_object_or_404(RoomPlan, id=1)
+	rooms = floor.roomplan_set.all() ## All the rooms in the chosen floor
+	optionTypes = get_object_or_404(RoomPlan, id=1).optionTypes.all() # All the option types for the chosen room
+
+	context = {'model':model, 'floor':floor, 'room':room, 'rooms':rooms, 'optionTypes':optionTypes , "optionsToLoad":"" }
+	return render(request, 'index.html', context)
+
+def logout(request):
+
+	auth_logout(request)
+	return redirect('login.html')
+
+
+def register(request):
+
+	context = {}
+
+	# In case the user reloaded the page
+	if request.method == 'GET':
+		context['form'] = Registration()
+		return render(request, 'register.html', context)
+
+
+	form = Registration(request.POST)
+	context['form'] = form
+	if not form.is_valid():
+		return render(request, 'register.html', context)
+
+	newUser = User.objects.create_user(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+	newUser.is_active = True # CHANGE LATER
+
+	newUser.save()
+
+	context['form'] = Login()
+	return render(request, "login.html", context)
+
+
 
 
